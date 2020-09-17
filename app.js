8c093a2bc411dbecaf7213aa26941218ebe30019
Sean Gibson <seanthomasgibson@gmail.com>
2020-09-17 11:56:26 -0700

    added files back

---
+++ app.js
@@ -0,0 +1,14 @@
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = 0;
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`
+}
+
+// To change each letter independently
+const letters = document.querySelectorAll('.letter');
+const intervalId = setInterval(function() {
+  for (let letter of letters) {
+    letter.style.color = randomRGB();
+  }
+}, 1000);
